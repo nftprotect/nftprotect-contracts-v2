86c6861a10437a565107b91c3bc70a38976534b7
Oleg Dubinkin <odubinkin@gmail.com>
2023-11-15 16:37:33 +0700

    deploy works

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,46 @@
-import { formatEther, parseEther } from "viem";
+import { formatEther, parseEther, GetContractReturnType } from "viem";
 import hre from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = BigInt(currentTimestampInSeconds + 60);
+const aregAddress = "0x6b5f8057679F6F35Efd1Ffb9EE90cF71f900bC38";
+const didAddress = "0x06da1f111d42BDf68b5AdA620459385D5ae2E3B1";
 
-  const lockedAmount = parseEther("0.001");
-
-  const lock = await hre.viem.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  console.log(
-    `Lock with ${formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+async function deployNFTProtect2() {
+  const nftProtect2 = await hre.viem.deployContract("NFTProtect2");
+  return nftProtect2;
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function deployUserRegistry(arbitratorRegistry: GetContractReturnType, did: GetContractReturnType, nftProtect2: GetContractReturnType) {
+  const userRegistry = await hre.viem.deployContract("UserRegistry", [arbitratorRegistry.address, did.address, nftProtect2.address]);
+  return userRegistry;
+}
+
+async function deployRequestsHub(arbitratorRegistry: GetContractReturnType, nftProtect2: GetContractReturnType) {
+  const requestsHub = await hre.viem.deployContract("RequestsHub", [arbitratorRegistry.address, nftProtect2.address]);
+  return requestsHub;
+}
+
+async function deployProtectorFactory721(nftProtect2: GetContractReturnType) {
+  const protectorFactory721 = await hre.viem.deployContract("ProtectorFactory721", [nftProtect2.address]);
+  return protectorFactory721;
+}
+
+async function main() {
+  const arbitratorRegistry = await hre.viem.getContractAt("ArbitratorRegistry", aregAddress)
+  const did = await hre.viem.getContractAt("UserDIDDummyAllowAll", didAddress)
+  
+  const nftProtect2 = await deployNFTProtect2();
+  console.log(`NFTProtect2 deployed to ${nftProtect2.address}`);
+
+  const userRegistry = await deployUserRegistry(arbitratorRegistry, did, nftProtect2);
+  console.log(`UserRegistry deployed to ${userRegistry.address}`);
+
+  const requestsHub = await deployRequestsHub(arbitratorRegistry, nftProtect2);
+  console.log(`RequestsHub deployed to ${requestsHub.address}`);
+
+  const protectorFactory721 = await deployProtectorFactory721(nftProtect2);
+  console.log(`ProtectorFactory721 deployed to ${protectorFactory721.address}`);
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
