92ecde0a5eb545b2dc3c5093fe3c9bfe831fe19d
Oleg Dubinkin <odubinkin@gmail.com>
2023-11-17 14:59:19 +0700

    more configure logic

---
+++ contracts.config.ts
@@ -0,0 +1,7 @@
+// Technical owner address that will be used for protected collections
+export const technicalOwner = '0xD05B13E2C5E0e1071442F9F7C99beE136ecced43'
+// Account who can update MetaEvidences
+export const metaEvidenceLoader = '0xD05B13E2C5E0e1071442F9F7C99beE136ecced43'
+// Fee for protecting (Wei)
+export const basicFeeWei = 10000000000000000n
+export const ultraFeeWei = 20000000000000000n
--- scripts/configure.ts
+++ scripts/configure.ts
@@ -1,5 +1,12 @@
 import { readFileSync, existsSync } from 'fs';
 import hre from "hardhat";
+import { PublicClient } from "viem";
+import { 
+    technicalOwner,
+    metaEvidenceLoader,
+    basicFeeWei, 
+    ultraFeeWei 
+} from '../contracts.config';
 
 const contractsFilePath = './contracts.json'; // Path to your JSON file
 const arbitratorsFilePath = './arbitrators.json'; // Path to your arbitrators file
@@ -9,6 +16,22 @@ const networkName = hre.network.name;
 let contractsData = existsSync(contractsFilePath) ? JSON.parse(readFileSync(contractsFilePath, 'utf-8')) : {};
 let arbitratorsData = existsSync(arbitratorsFilePath) ? JSON.parse(readFileSync(arbitratorsFilePath, 'utf-8')) : {};
 let networkData = contractsData[networkName] || {};
+let client: PublicClient;
+
+async function processTransaction(hash :`0x${string}`) {
+    if (client) {
+        console.log(`Waiting for transaction receipt ( ${hash} )...`)
+        const receipt = await client.waitForTransactionReceipt({hash})
+        if (receipt.status === 'success') {
+            console.log('Transaction successfull')
+        } else {
+            console.log('Transaction unsuccessfull:', receipt)
+            throw Error(`Transaction error: ${hash}`)
+        }
+    }
+}
+
+// ArbitratorRegistry
 
 async function configureArbitratorRegistry() {
     let arbitratorData = arbitratorsData[networkName];
@@ -31,9 +54,12 @@ async function configureArbitratorRegistry() {
         console.log(`Setting Arbitrator...`);
     }
 
-    await contract.write.addArbitrator([arbitratorData.name, arbitratorData.address, arbitratorData.extraData]);
+    const hash = await contract.write.addArbitrator([arbitratorData.name, arbitratorData.address, arbitratorData.extraData]);
+    await processTransaction(hash)
     return contract
 }
+
+// NFTProtect 2
 
 async function setNFTProtectRequestHub() {  
     if (!networkData["NFTProtect2"] || !networkData["RequestsHub"]) {
@@ -47,7 +73,8 @@ async function setNFTProtectRequestHub() {
         return contract;
     }
     console.log(`Setting RequestsHub ${networkData["RequestsHub"]}...`);
-    await contract.write.setRequestHub([networkData["RequestsHub"]]);
+    const hash = await contract.write.setRequestHub([networkData["RequestsHub"]]);
+    await processTransaction(hash)
     return contract;
 }
   
@@ -65,7 +92,8 @@ async function setNFTProtectUserRegistry() {
     }
     
     console.log(`Setting UserRegistry ${networkData["UserRegistry"]}...`);
-    await contract.write.setUserRegistry([networkData["UserRegistry"]]);
+    const hash = await contract.write.setUserRegistry([networkData["UserRegistry"]]);
+    await processTransaction(hash)
     return contract
 }
 
@@ -78,9 +106,58 @@ async function registerProtectorFactory() {
     const factoryStatus = await contract.read._factories([networkData["ProtectorFactory721"]]);
     if (factoryStatus === 0n) {
         console.log(`Registering ProtectorFactory721 ${networkData["ProtectorFactory721"]}...`);
-        await contract.write.registerProtectorFactory([networkData["ProtectorFactory721"]]);
+        const hash = await contract.write.registerProtectorFactory([networkData["ProtectorFactory721"]]);
+        await processTransaction(hash)
     } else {
         console.log(`ProtectorFactory721 ${networkData["ProtectorFactory721"]} is already registered`);
+    }
+    return contract;
+}
+
+async function setTechnicalOwner() {
+    if (!networkData["NFTProtect2"]) {
+        throw Error("NFTProtect2 contract address not found in contracts.json");
+    }
+
+    const contract = await hre.viem.getContractAt("NFTProtect2", networkData["NFTProtect2"]);
+    const currentTechnicalOwner = await contract.read._technicalOwner();
+
+    if (currentTechnicalOwner.toLowerCase() !== technicalOwner.toLowerCase()) {
+        console.log(`Setting technicalOwner to ${technicalOwner}...`);
+        const hash = await contract.write.setTechnicalOwner([technicalOwner]);
+        await processTransaction(hash)
+    } else {
+        console.log(`TechnicalOwner is already set to ${technicalOwner}`);
+    }
+    return contract;
+}
+
+// UserRegistry
+
+async function configureUserRegistryFees() {
+    if (!networkData["UserRegistry"]) {
+        throw Error("UserRegistry contract address not found in contracts.json");
+    }
+
+    const contract = await hre.viem.getContractAt("UserRegistry", networkData["UserRegistry"]);
+
+    const currentBasicFee = await contract.read._feeWei([0]);
+    const currentUltraFee = await contract.read._feeWei([1]);
+
+    if (currentBasicFee !== basicFeeWei) {
+        console.log(`Setting basicFeeWei to ${basicFeeWei}...`);
+        const hash = await contract.write.setFee([0, basicFeeWei]);
+        await processTransaction(hash)
+    } else {
+        console.log(`BasicFeeWei is already set to ${basicFeeWei}`);
+    }
+
+    if (currentUltraFee !== ultraFeeWei) {
+        console.log(`Setting ultraFeeWei to ${ultraFeeWei}...`);
+        const hash = await contract.write.setFee([1, ultraFeeWei]);
+        await processTransaction(hash)
+    } else {
+        console.log(`UltraFeeWei is already set to ${ultraFeeWei}`);
     }
 
     return contract;
@@ -88,15 +165,23 @@ async function registerProtectorFactory() {
 
 async function main() {
     try {
-        console.log(`1. ArbitratorRegistry:`);
-        const arbRegistry = await configureArbitratorRegistry();
-        console.log(`ArbitratorRegistry ${arbRegistry.address} configured successfully`);
-        console.log(`2. NFTProtect2:`);
-        await setNFTProtectRequestHub();
-        await setNFTProtectUserRegistry();
-        const nftProtect = await registerProtectorFactory();
-        console.log(`NFTProtect2 ${nftProtect.address} configured successfully`);
-
+        client = await hre.viem.getPublicClient();
+        if (client) {
+            console.log(`1. ArbitratorRegistry:`);
+            const arbRegistry = await configureArbitratorRegistry();
+            console.log(`ArbitratorRegistry ${arbRegistry.address} configured successfully`);
+            console.log(`2. NFTProtect2:`);
+            await setNFTProtectRequestHub();
+            await setNFTProtectUserRegistry();
+            await setTechnicalOwner();
+            const nftProtect = await registerProtectorFactory();
+            console.log(`NFTProtect2 ${nftProtect.address} configured successfully`);
+            console.log(`3. UserRegistry:`);
+            await configureUserRegistryFees();
+            console.log(`UserRegistry configured successfully`);
+        } else {
+            throw Error('No client configured')
+        }
     } catch (error) {
         console.error(error);
         process.exitCode = 1;
