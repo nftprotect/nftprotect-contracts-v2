5733906a0f3c0d41d3821683ee9f3a10a4a011fd
Oleg Dubinkin <odubinkin@gmail.com>
2023-11-17 21:48:15 +0700

    rename secret

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
-const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
 
 const config: HardhatUserConfig = {
@@ -21,7 +21,7 @@ const config: HardhatUserConfig = {
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY]
+      accounts: [PRIVATE_KEY]
     }
   },
   etherscan: {
